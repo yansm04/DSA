0137211161fe74a1c288526dd211bbf4a72cbb11
nomom <clementlaukaiming@gmail.com>
2025-03-12 16:17:59 +0800

    entity package

---
+++ Assignment/src/entity/Applicant.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entity;
+import adt.DoublyLinkedList;
+import adt.ListInterface;
+
+/**
+ *
+ * @author Acer
+ */
+public class Applicant {
+    
+    private String userID;
+    private String name;
+    private String email;
+    private int age;
+    private ListInterface<String> skills = new DoublyLinkedList<>(); 
+    private ListInterface<String> workingExperience = new DoublyLinkedList<>();
+    private ListInterface<String> education = new DoublyLinkedList<>();
+    //private ListInterface<Application> application = new DoublyLinkedlist<>();
+    
+    
+    public Applicant
+}
